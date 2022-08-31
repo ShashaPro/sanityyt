69d29fab2226f14002323109073500413932cae5
ShashaPro <shashaprogrammings@gmail.com>
2022-08-31 13:18:30 +0300

    sanityyt done

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -193,9 +193,12 @@ export default Post;
 
 export const getStaticPaths = async () => {
     const query = 
-         `*[_type == "post" && slug.current]{
-        
-        }`;
+         `*[_type == "post"] {
+            _id,
+            slug {
+              current
+           }
+          }`;
 
     const posts = await sanityClient.fetch(query);
 
@@ -214,7 +217,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
     const query = 
-               `*[_type == "post" && slug.current == "my-first-post"][0]{
+               `*[_type == "post" && slug.current == $slug][0]{
                 _id,
               _createdAt,
               title,
